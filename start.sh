89343a402daa0a60e9b896937e23275d2c6af7c4
adfindlater <findlater.alexander@gmail.com>
2022-03-05 19:50:41 -0500

    m

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-libcamera-vid -t 0 --inline --listen -o tcp://0.0.0.0:8080 --width 800 --height 600 --codec mjpeg -q 100 &
+libcamera-vid -t 0 --inline --listen -o tcp://0.0.0.0:8080 --width 800 --height 600 --codec mjpeg -q 40 &
 python3 -m pybaby.app
