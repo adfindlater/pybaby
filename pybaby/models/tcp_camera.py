f7ad89c7c05fae265c94d05c2099ed980cdf772c
adfindlater <findlater.alexander@gmail.com>
2022-03-05 19:39:37 -0500

    update

--- pybaby/models/tcp_camera.py
+++ pybaby/models/tcp_camera.py
@@ -2,7 +2,7 @@ import cv2
 from pybaby.models.base_camera import BaseCamera
 
 capture = cv2.VideoCapture("tcp://0.0.0.0:8080")
-
+capture.set(cv2.CAP_PROP_BUFFERSIZE, 5)
 
 class TCPCamera(BaseCamera):
     @staticmethod
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-libcamera-vid -t 0 --inline --listen -o tcp://0.0.0.0:8080 --width 800 --height 600 --codec mjpeg -q 50 &
+libcamera-vid -t 0 --inline --listen -o tcp://0.0.0.0:8080 --width 800 --height 600 --codec mjpeg -q 100 &
 python3 -m pybaby.app
